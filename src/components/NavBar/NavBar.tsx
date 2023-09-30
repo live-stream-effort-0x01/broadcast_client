c0bd1d5f93295071975748c3c5c9b1ae0ff1d497
justin <justinsweng@gmail.com>
2023-09-30 04:32:53 -0400

    add stream url

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,19 +1,26 @@
 import { createSignal, Show } from 'solid-js';
 
 export default function NavBar() {
-    return (
-        <header>
+  return (
+    <header>
 
-        <Show
+      <Show
         when={loggedIn()}
-        fallback={            
-        <nav class="navigation">
-          <a href="#" class="navigation-element">Login</a>
-          <a href="#" class="navigation-element">Sign-Up</a>
-        </nav>
+        fallback={
+          <nav class="navigation">
+            <a href="#" class="navigation-element">Login</a>
+            <a href="#" class="navigation-element">Sign-Up</a>
+          </nav>
         }
       >
+
+        <nav id="inbetween" class="navigation">
+          <form action={import.meta.env.VITE_STREAM_URL}>
+            <button class="stream-button" type="submit">Start Streaming</button>
+          </form>
+          <a href="#" class="navigation-element">Username</a>
+        </nav>
       </Show>
-      </header>
-    );
+    </header>
+  );
 }
