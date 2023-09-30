a360c7f3784eebc98a09926a69a532b0715d6758
justin <justinsweng@gmail.com>
2023-09-30 04:26:24 -0400

    update reference

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,16 +1,18 @@
+import { createSignal, Show } from 'solid-js';
+
 export default function NavBar() {
     return (
         <header>
 
         <Show
         when={loggedIn()}
-        fallback={            <nav class="navigation">
-        <a href="#" class="navigation-element">Login</a>
-        <a href="#" class="navigation-element">Sign-Up</a>
-    </nav>
-    }
+        fallback={            
+        <nav class="navigation">
+          <a href="#" class="navigation-element">Login</a>
+          <a href="#" class="navigation-element">Sign-Up</a>
+        </nav>
+        }
       >
-        <button onClick={toggle}>Log out</button>
       </Show>
       </header>
     );
