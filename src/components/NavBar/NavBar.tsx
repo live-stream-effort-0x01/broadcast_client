02f1b4c5b8a0fe8d5395030ee14534e2780cffec
Quan Doan <quan9824@gmail.com>
2023-10-05 10:21:34 +0700

    add navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,31 @@
 import { createSignal, Show } from 'solid-js';
-
+import './NavBar.css'
 export default function NavBar() {
   const [loggedIn, setLoggedIn] = createSignal(false);
+  const [live, setLive] = createSignal(true);
   return (
-    <header>
-
+    <header class='header-wapper'>
+     
       <Show
         when={loggedIn()}
         fallback={
-          <nav class="navigation">
-            <a href="#" class="navigation-element">Login</a>
+          <nav class="navigation flex-end">
             <a href="#" class="navigation-element">Sign-Up</a>
+            <a href="#" class="navigation-element">Login</a>
           </nav>
         }
       >
 
-        <nav id="inbetween" class="navigation">
+        <nav id="inbetween" class="navigation between ">
           <form action={import.meta.env.VITE_STREAM_URL}>
-            <button class="stream-button" type="submit">Start Streaming</button>
+            <button class={live()? "stream-button yellow ":'stream-button green' }type="submit">{live() ? 'Continute Streaming':'Start Streaming'}</button>
           </form>
           <a href="#" class="navigation-element">Username</a>
         </nav>
       </Show>
+
+     
+      <div class='header-line'></div>
     </header>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,13 +6,14 @@ export default function Home() {
     <main class='home-warrper'>
       <div class='home-header'><NavBar/></div>
       <div class='home-container'>    
-        <h1 class="demo-title">Current Broadcasts</h1>
+        {/* <h1 class="demo-title">Current Broadcasts</h1>
           <div class="cards">
               <div class="card">
                   <img src="images/broadcast.png" alt="broadcast image" width="320" height="240" class="card-image" />
                   <h1 class="card-title">Card 1</h1>
               </div>
-          </div></div>
+          </div> */}
+      </div>
       </main>
   );
 }
