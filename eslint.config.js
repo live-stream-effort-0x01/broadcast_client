630603ba7ce35ed6c4d73a62e756d5462444cb2e
Quan Doan <quan9824@gmail.com>
2025-02-17 02:24:34 -0800

    push missing updates

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+export default {
+  ignores: ['dist'],
+  overrides: [
+    {
+      files: ['**/*.{js,jsx}'],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+        parserOptions: {
+          ecmaVersion: 'latest',
+          ecmaFeatures: { jsx: true },
+          sourceType: 'module',
+        },
+      },
+      settings: { react: { version: '18.3' } },
+      plugins: {
+        react,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+      },
+      rules: {
+        ...js.configs.recommended.rules,
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
+        ...reactHooks.configs.recommended.rules,
+        'react/jsx-no-target-blank': 'off',
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+        ],
+      },
+    },
+  ],
+};
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'), // Alias cho thư mục src
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name].[hash].js',
+        chunkFileNames: 'js/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
